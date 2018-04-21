a02df8be7799e3f75c3ef133efdfc70f6a4ae219
David Thorpe <djt@mutablelogic.com>
2018-04-21 15:11:44 +0100

    Updated code to use a merged channel

--- cmd/rpc/service/service.go
+++ cmd/rpc/service/service.go
@@ -13,7 +13,6 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
-	"time"
 
 	// Frameworks
 	gopi "github.com/djthorpe/gopi"
@@ -64,8 +63,10 @@ type Service struct {
 }
 
 type service struct {
-	log  gopi.Logger
-	done *evt.PubSub
+	log    gopi.Logger
+	done   *evt.PubSub
+	merger *evt.EventMerger
+	codecs []remotes.Codec
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -78,6 +79,8 @@ func (config Service) Open(log gopi.Logger) (gopi.Driver, error) {
 	this := new(service)
 	this.log = log
 	this.done = evt.NewPubSub(1)
+	this.codecs = make([]remotes.Codec, 0, 10)
+	this.merger = evt.NewEventMerger()
 
 	// Register service with server
 	config.Server.Register(this)
@@ -87,11 +90,16 @@ func (config Service) Open(log gopi.Logger) (gopi.Driver, error) {
 }
 
 func (this *service) Close() error {
-	this.log.Debug("<grpc.service.remotes>Close{}")
+	this.log.Debug("<grpc.service.remotes>Close{ codecs=%v }", this.codecs)
 
 	// Close done
 	this.done.Close()
 	this.done = nil
+
+	// Release codecs
+	this.merger.Close()
+	this.merger = nil
+	this.codecs = nil
 
 	// Success
 	return nil
@@ -102,6 +110,10 @@ func (this *service) Close() error {
 
 func (this *service) registerCodec(codec remotes.Codec) {
 	this.log.Debug2("<grpc.service.remotes>RegisterCodec{ codec=%v }", codec)
+
+	// Append codecs and subscibe
+	this.codecs = append(this.codecs, codec)
+	this.merger.Add(codec.Subscribe())
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -123,7 +135,8 @@ func (this *service) GRPCHook() reflect.Value {
 // RPC SERVICE REQUESTS
 
 func (this *service) Receive(_ *pb.EmptyRequest, stream pb.Remotes_ReceiveServer) error {
-	timer := time.Tick(5 * time.Second)
+	// Subscribe to the merger channel
+	input_events := this.merger.Subscribe()
 
 	// Get a channel we will use for breaking the loop
 	done := this.done.Subscribe()
@@ -132,21 +145,22 @@ func (this *service) Receive(_ *pb.EmptyRequest, stream pb.Remotes_ReceiveServer
 FOR_LOOP:
 	for {
 		select {
-		case <-timer:
-			reply := toProtobufInputEvent()
+		case evt := <-input_events:
+			reply := toProtobufInputEvent() // TODO
 			if err := stream.Send(reply); err != nil {
 				this.log.Warn("Receive: error sending: %v: closing request", err)
 				break FOR_LOOP
 			} else {
-				this.log.Info("Sent: %v", reply)
+				this.log.Info("Sent: %v", evt)
 			}
 		case <-done:
 			break FOR_LOOP
 		}
 	}
 
-	// Unsubscribe from done signal
+	// Unsubscribe
 	this.done.Unsubscribe(done)
+	this.merger.Unsubscribe(input_events)
 
 	// Return success
 	return nil
@@ -156,7 +170,7 @@ FOR_LOOP:
 // STRINGIFY
 
 func (this *service) String() string {
-	return fmt.Sprintf("grpc.service.remotes{}")
+	return fmt.Sprintf("grpc.service.remotes{ codecs=%v }", this.codecs)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
