8e4ebf46e58f5a3053f3c1ea687a133567d77dc9
David Thorpe <djt@mutablelogic.com>
2018-04-26 08:55:31 +0100

    Updated build script and gitignore

--- cmd/build-rpc.sh
+++ cmd/build-rpc.sh
@@ -6,6 +6,7 @@
 CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GO=`which go`
 PROTOC=`which protoc`
+PROTOC_GEN_GO=`which protoc-gen-go`
 LDFLAGS="-w -s"
 cd "${CURRENT_PATH}/.."
 
@@ -20,6 +21,19 @@ if [ "${GO}" == "" ] || [ ! -x ${GO} ] ; then
   echo "go not installed or executable" >&2
   exit -1
 fi
+if [ ! -x "${PROTOC}" ] ; then
+  echo "protoc not installed or executable" >&2
+  exit -1
+fi
+
+##############################################################
+# gRPC Plugin compile
+
+if [ ! -x "${PROTOC_GEN_GO}" ]; then
+  echo "go get -u github.com/golang/protobuf/protoc-gen-go" >&2
+  go get -u google.golang.org/grpc || exit 1
+  go get -u github.com/golang/protobuf/protoc-gen-go || exit 1
+fi
 
 ##############################################################
 # Install RPC binaries
@@ -29,8 +43,6 @@ COMMANDS=(
   rpc/remotes-client.go
 )
 
-echo "go get -u github.com/golang/protobuf/protoc-gen-go" >&2
-go get -u github.com/golang/protobuf/protoc-gen-go || exit 1
 echo "go generate github.com/djthorpe/remotes/protobuf"
 go generate -x github.com/djthorpe/remotes/protobuf || exit 1
 for COMMAND in ${COMMANDS[@]}; do
